df00e9768ba16a5229df6e061da10a278d5d18b9
TioDark2023 <wheslley.lucas@hotmail.com>
2025-08-08 15:13:28 +0000

    Adicionei comparacao e contas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,16 +4,26 @@ int main(){
 
     // Primeira carta 
     int carta=1;
-    int Populacao = 7.330483 ;
-    int NumeroTuristicos= 86;
+    int Populacao=7.330483 ;
+    int NumeroTuristicos=86;
     char Estado='G';
     char Codigarta[10]="G01";
     char NomeCidade[20]="Goiais";
-    float Areakm= 340.086;
-    float Pib= 336.7;
-    float pibper= Pib / Populacao;
-    float Denspo= Populacao / Areakm; 
-      // Segunda carta 
+    float Areakm=340.086;
+    float Pib=336.7;
+    float pibper=Pib / Populacao;
+    float Denspo=Populacao / Areakm;
+    float pop=7.3;                       // popuulacao 
+    float notu=86;                      // numeros turisticos 
+    float akm=340;                     // area km2 
+    float pib=336;                    // pib 
+    float pibperc= pib/pop;          //pib perc
+    float den= pop/akm;             // dencidade 
+    float poder;
+    poder=pop+notu+akm+pib+(den/1)+pibperc;  // Soma total dos float 
+    // Fim do primeiro cod
+
+    // Segunda carta 
     int carta2=2;
     int Populacao2= 3.833712;
     int NumeroTuristicos2= 45;
@@ -23,39 +33,64 @@ int main(){
     float Areakm2= 46.095;
     float Pib2= 51.1;
     float pibper2= Pib2 / Populacao2;
-    float Denspo2= Populacao2 / Areakm2; 
-
-
-// Info da primeira carta. 
-   printf("Carta:%d\n",carta); // qual o número da carta 
-   printf("Estado:%c\n",Estado); // o nome do estado.  
-   printf("Código:%s\n",Codigarta); // código da carta. 
-   printf("Nome Da Cidade:%s\n",NomeCidade); // nome da cidade. 
-   printf("População:%d\n",Populacao); // quantidade da população.  
-   printf("Área:%.3f km2\n",Areakm); // quantidade da área. 
-   printf("PIB:%.2f bilhoes de reais\n",Pib); // PIB 
-   printf("Numero de Pontos Turísticos:%d\n",NumeroTuristicos); // quantidades de pontos turísticos. 
-   printf("Densidade Populacional:%.2f hab/km2\n", Denspo); // densidade populacional
-   printf("PIB per Capita:%.2f reais\n", pibper);  // pib percapta 
-
- 
-   printf ("\n");
-
-   // Info da segunda carta. 
-   printf("Carta:%d\n",carta2); // qual o número da carta. 
-   printf("Estado:%c\n",Estado2); // o nome do estado.  
-   printf("Código:%s\n",Codigocarta2); // código da carta. 
-   printf("Nome Da Cidade:%s\n",NomeCidade2); // nome da cidade.  
-   printf("População:%d\n",Populacao2); // quantidade da população.  
-   printf("Área:%.2f km2\n",Areakm2); // quantidade da área.
-   printf("PIB:%.2f bilhoes de reais\n",Pib2); // PIB 
-   printf("Numero De Ponts Turísticos:%d\n",NumeroTuristicos2); // quantidades de pontos turísticos.
-  printf("Densidade Populacional:%.2f hab/km2\n", Denspo2); // densidade populacional
-   printf("PIB per Capita:%.2f reais\n", pibper2);    // pib percapta 
-
+    float Denspo2= Populacao2 / Areakm2;
+    float pop2=3.8;                            // popuulacao 
+    float notu2=45;                           // numeros turisticos 
+    float akm2=46;                           // area km2 
+    float pib2=51.1;                        // pib 
+    float pibperc2= pib2/pop2;             //pib perc
+    float den2= pop2/akm2;                // dencidade 
+    float poder2;
+   poder2=pop2+notu2+akm2+pib2+(den2/1)+pibperc2;  // Soma total dos float 
+   // Fim do segundo cod 
+   
+   
 
    printf("\n");
+// Info da primeira carta. 
+   printf("Carta:%d\n",carta);                                   // qual o número da carta 
+   printf("Estado:%c\n",Estado);                                // o nome do estado.  
+   printf("Código:%s\n",Codigarta);                            // código da carta. 
+   printf("Nome Da Cidade:%s\n",NomeCidade);                  // nome da cidade. 
+   printf("População:%d\n",Populacao);                       // quantidade da população.  
+   printf("Área:%.3f km2\n",Areakm);                        // quantidade da área. 
+   printf("PIB:%.2f bilhoes de reais\n",Pib);              // PIB 
+   printf("Numero de Pontos Turísticos:%d\n",NumeroTuristicos); // quantidades de pontos turísticos. 
+   printf("Densidade Populacional:%.2f hab/km2\n", Denspo);    // densidade populacional
+   printf("PIB per Capita:%.2f reais\n", pibper);             // pib percapta 
+   printf("Poder:%.2f\n",poder);                             // Total De Poder da carta 
+   // Fim do primeiro cod 
+   printf("\n");
+   // Info da segunda carta. 
+   printf("Carta:%d\n",carta2);                             // qual o número da carta. 
+   printf("Estado:%c\n",Estado2);                          // o nome do estado.  
+   printf("Código:%s\n",Codigocarta2);                    // código da carta. 
+   printf("Nome Da Cidade:%s\n",NomeCidade2);            // nome da cidade.  
+   printf("População:%d\n",Populacao2);                 // quantidade da população.  
+   printf("Área:%.2f km2\n",Areakm2);                  // quantidade da área.
+   printf("PIB:%.2f bilhoes de reais\n",Pib2);        // PIB 
+   printf("Numero De Ponts Turísticos:%d\n",NumeroTuristicos2);  // quantidades de pontos turísticos.
+   printf("Densidade Populacional:%.2f hab/km2\n", Denspo2);    // densidade populacional
+   printf("PIB per Capita:%.2f reais\n", pibper2);             // pib percapta 
+   printf("Poder:%.2f\n",poder2);                             // Total De Poder da carta 
+   printf("\n");
+   //fim do segundo cod  
 
+  //comparacao das cartas Verificar todos e corrigir tudo 
+   printf("comparacao entre cartas\n");
+   printf("\n");
+   printf("Se o Resultado der ( 0 ) A carta 1 Ganhou e Se Der ( 1 ) A Carta 2 Ganhou:\n");
+   printf("\n");
+ 
+   printf("ppopulacao : %d\n", Populacao>Populacao2);
+   printf("Areakm2 : %d\n", Areakm>Areakm2);
+   printf("Pib : %d\n", Pib>Pib2);
+   printf("Pontos Turisticos : %d\n", NumeroTuristicos>NumeroTuristicos2);
+   printf("Densidade Populacional : %d\n", Denspo>Denspo2);
+   printf("PIB per Capita : %d\n", pibper>pibper2);
+   printf("Super Poder:%d\n",poder>poder2);
+   // fim do cod veri.. 
+  
 
    return 0;
 }
